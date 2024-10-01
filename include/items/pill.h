305e945ad1a32d72ad5b95e391a6c74b5a8b0a6e
Vaykan <unnamed@gmail.com>
2024-10-01 16:44:45 +0300

    add pill item

---
+++ include/items/pill.h
@@ -0,0 +1,12 @@
+#ifndef BUCKSHOT_ROULETTE_PILL_H
+#define BUCKSHOT_ROULETTE_PILL_H
+
+#include "item.h"
+
+class Pill : public Item {
+public:
+    Pill();
+};
+
+
+#endif
---
+++ src/items/pill.cpp
@@ -0,0 +1,5 @@
+#include "items/pill.h"
+
+Pill::Pill() {
+    itemType = PILL;
+}
