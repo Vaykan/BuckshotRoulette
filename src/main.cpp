c0185d514e96f3b330dbe9651eaf9e34fd0676d6
Vaykan <unnamed@gmail.com>
2024-10-01 16:05:01 +0300

    main clear

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,1 @@
-#include <iostream>
-
-#include "shell.h"
-#include "shotgun.h"
-
-int main() {
-   Shotgun shotgun;
-
-   shotgun.loading();
-
-    for (int i = 0; i < 8; ++i) {
-        shotgun.pumping();
-    }
-
-    return 0;
-}
+int main(){}
