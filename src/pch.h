28d47fab0ea87912dbfac483993cae224b835ece
Vaykan <unnamed@gmail.com>
2025-05-12 17:55:49 +0300

    pch.h add

---
+++ src/pch.h
@@ -0,0 +1,13 @@
+#ifndef BUCKSHOT_ROULETTE_EXE_PCH_H
+#define BUCKSHOT_ROULETTE_EXE_PCH_H
+
+#include <wx/wx.h>
+#include <wx/wxprec.h>
+#include <wx/frame.h>
+#include <wx/panel.h>
+#include <wx/button.h>
+#include <wx/sizer.h>
+#include <wx/textctrl.h>
+#include <wx/stattext.h>
+
+#endif
