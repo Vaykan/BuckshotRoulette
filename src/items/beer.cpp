e9c473e2fa2cf33c7e0a80e1610f8e427343106b
Vaykan <unnamed@gmail.com>
2025-05-15 13:28:36 +0300

    fix beer.cpp text output

--- src/items/beer.cpp
+++ src/items/beer.cpp
@@ -7,10 +7,6 @@ Beer::Beer() {
 
 void Beer::use() {
     owner->getMyFrame()->getTextCtrl()->AppendText(owner->getName());
-    owner->getMyFrame()->getTextCtrl()->AppendText(" used Beer\n\nShell ejected: ");
-    if (owner->getShotgun()->getBackShell() == LIVE)
-        owner->getMyFrame()->getTextCtrl()->AppendText("LIVE\n\n");
-    else
-        owner->getMyFrame()->getTextCtrl()->AppendText("BLANK\n\n");
+    owner->getMyFrame()->getTextCtrl()->AppendText(" used Beer\n\n");
     owner->getShotgun()->pumping();
 }
