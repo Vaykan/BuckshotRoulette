1c7077c8f9bf8cd53ddd7b977b909d43ad267358
Vaykan <unnamed@gmail.com>
2025-06-26 22:23:44 +0300

    Phone fix

--- src/items/phone.cpp
+++ src/items/phone.cpp
@@ -9,11 +9,11 @@ void Phone::use() {
     std::cout << owner->getName() << " used Phone\n\n";
     std::vector<ShellType>* magazine = &owner->getShotgun()->getMagazine();
     if (magazine->size() >= 2) {
-        int index = rGetNum(1, magazine->size() - 1);
+        int index = rGetNum(0, magazine->size() - 1);
         if ((*magazine)[index] == LIVE)
-            std::cout << index + 1 << " shell LIVE\n\n";
+            std::cout << -(index - magazine->size()) << " shell LIVE\n\n";
         else
-            std::cout << index + 1 << " shell BLANK\n\n";
+            std::cout << -(index - magazine->size()) << " shell BLANK\n\n";
     } else {
         std::cout << "How Unfortunate...\n\n";
     }
